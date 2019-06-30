13d404078091ed23351030b18e58e72eb7f6e0eb
vithyasri <52270841+vithyasri@users.noreply.github.com>
2019-06-30 16:32:43 +0530

    Create vith.py

---
+++ vith.py
@@ -0,0 +1,6 @@
+# your code goes here
+ch = input("Enter a character: ")
+if((ch>='a' and ch<= 'z') or (ch>='A' and ch<='Z')):
+    print(ch, "is an Alphabet")
+else:
+    print(ch, "is not an Alphabet")
